b06757a1405fe6527c75ed73d08d1fab781efb97
root <root@localhost.localdomain>
2020-06-09 17:06:42 +0800

    build 沒有加 add-host 把npmjs.org網域加入會導致建置失敗

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@ set -ex
 # SET THE FOLLOWING VARIABLES
 USERNAME=gjovanov
 IMAGE=roomler
-docker build --network=host -t $USERNAME/$IMAGE:latest .
+docker build --network=host --add-host registry.npmjs.org:104.16.26.35 -t $USERNAME/$IMAGE:latest .
